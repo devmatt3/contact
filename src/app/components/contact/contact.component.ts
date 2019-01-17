e6d3d148261c16c22bd6f2875d3ae006918955c6
Matt Armfield <armfield@airtype.com>
2019-01-17 17:04:42 -0500

    add in input field

---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  inputChange(input){
+    if("" != input){
+
+    }
+  }
+
+
+
+}
